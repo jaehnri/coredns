13142cf87f6556187064dcf98fb2072bf7fe3c5d
jaehnri <joao.henri.cr@gmail.com>
2023-07-06 17:29:07 -0300

    Move QUIC's STREAM FIN to DoQWriter.Close()
    Signed-off-by: jaehnri <joao.henri.cr@gmail.com>

--- core/dnsserver/quic.go
+++ core/dnsserver/quic.go
@@ -26,7 +26,20 @@ func (w *DoQWriter) WriteMsg(m *dns.Msg) error {
 	}
 
 	_, err = w.Write(bytes)
-	return err
+	if err != nil {
+		return err
+	}
+
+	return w.Close()
+}
+
+// Close sends the STREAM FIN signal.
+// The server MUST send the response(s) on the same stream and MUST
+// indicate, after the last response, through the STREAM FIN
+// mechanism that no further data will be sent on that stream.
+// See https://www.rfc-editor.org/rfc/rfc9250#section-4.2-7
+func (w *DoQWriter) Close() error {
+	return w.stream.Close()
 }
 
 // AddPrefix adds a 2-byte prefix with the DNS message length.
@@ -39,7 +52,6 @@ func AddPrefix(b []byte) (m []byte) {
 }
 
 // These methods implement the dns.ResponseWriter interface from Go DNS.
-func (w *DoQWriter) Close() error          { return nil }
 func (w *DoQWriter) TsigStatus() error     { return nil }
 func (w *DoQWriter) TsigTimersOnly(b bool) {}
 func (w *DoQWriter) Hijack()               {}
